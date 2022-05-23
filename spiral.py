0fa544c0c077c5096901a41dd04be64667e6e088
marykaldor <mek5752@psu.edu>
2022-05-23 16:52:20 -0400

    5/23

--- spiral.py
+++ spiral.py
@@ -17,7 +17,7 @@ import timeit
 start = timeit.default_timer()
 
 
-parameters = ["XI1", "XI2", "PITCH", "WIDTH", "XISPIN", "XISPOUT"]
+parameters = ["XI1", "XI2", "AMP", "AOBS", "PITCH", "WIDTH", "XISPIN", "XISPOUT"]
 parvaluesi = []
 parvaluesf = []
 j = 0
@@ -58,6 +58,8 @@ parvaluesf.append(xispin)
 parvaluesf.append(xispout)
 parvaluesf.append(parvaluesi[2])
 parvaluesf.append(parvaluesi[3])
+parvaluesf.append(parvaluesi[4])
+parvaluesf.append(parvaluesi[5])
 
 
 def cart2pol(x, y, xcenter, ycenter):
@@ -90,6 +92,7 @@ def pol2cart(r, theta):
 def rrangein(x, y, xcenter, ycenter):
     # Checks if some values of x and y are within a certain radius range
     # Radius range is determined by ximin and ximax above
+    # Inside of power law
     if ximin < cart2pol(x, y, xcenter, ycenter)[0] < xi_b + 1:
         return True
     else:
@@ -99,10 +102,12 @@ def rrangein(x, y, xcenter, ycenter):
 def rrangeout(x, y, xcenter, ycenter):
     # Checks if some values of x and y are within a certain radius range
     # Radius range is determined by ximin and ximax above
+    # Outside of power law
     if xi_b < cart2pol(x, y, xcenter, ycenter)[0] < ximax:
         return True
     else:
         return False
+
 
 
 # Create new image to build on
@@ -111,7 +116,7 @@ normlistinplaw = []
 normlistin = []
 normlistout = []
 values = []
-im = Image.new("RGB", (1500, 1000), "black")
+im = Image.new("RGB", (1800, 1400), "black")
 rgb_im = im.convert('RGB')
 [xs, ys] = rgb_im.size
 ximin = int(xispin) / 10
@@ -120,19 +125,19 @@ xi_b = 250
 
 for x in range(1, xs):
     for y in range(1, ys):
-        if rrangein(x, y, xs / 3, ys / 2):
+        if rrangein(x, y, xs / 3 + 100, ys / 2):
             # Linear
-            # normlistinplaw.append(cart2pol(x, y, xs / 3, ys / 2)[0] ** 0)
-            normlistinplaw.append(cart2pol(x, y, xs / 3, ys / 2)[0] ** 2)
+            # normlistinplaw.append(cart2pol(x, y, xs / 3 + 100, ys / 2)[0] ** 0)
+            normlistinplaw.append(cart2pol(x, y, xs / 3 + 100, ys / 2)[0] ** 2)
             # Log
-            # normlistin.append(math.log((cart2pol(x, y, xs / 3, ys / 2)[0] ** 1), 10))
-        if rrangeout(x, y, xs / 3, ys / 2):
+            # normlistin.append(math.log((cart2pol(x, y, xs / 3 + 100, ys / 2)[0] ** 1), 10))
+        if rrangeout(x, y, xs / 3 + 100, ys / 2):
             # This one has an extra factor at the beginning to make sure that the two are equal at xi_b
             # Linear
-            # normlistinplaw.append(cart2pol(x, y, xs / 3, ys / 2)[0] ** 0)
-            normlistinplaw.append(((xi_b ** 1) * (cart2pol(x, y, xs / 3, ys / 2)[0] ** 1)))
+            # normlistinplaw.append(cart2pol(x, y, xs / 3 + 100, ys / 2)[0] ** 0)
+            normlistinplaw.append(((xi_b ** 1) * (cart2pol(x, y, xs / 3 + 100, ys / 2)[0] ** 1)))
             # Log
-            # normlistin.append(math.log((((xi_b / ximin) ** 0.5) * (cart2pol(x, y, xs / 3, ys / 2)[0] ** 0.5)), 10))
+            # normlistin.append(math.log((((xi_b / ximin) ** 0.5) * (cart2pol(x, y, xs / 3 + 100, ys / 2)[0] ** 0.5)), 10))
 
 
 # General equation for spiral formation and location
@@ -165,17 +170,17 @@ def e(constant, xi, phi, phi0, p, a, delta):
 tally = 0
 for x in range(1, xs):
     for y in range(1, ys):
-        if rrangein(x, y, xs / 3, ys / 2):
-            # normlistin.append(e(normlistinplaw[tally], cart2pol(x, y, xs / 3, ys / 2)[0], (cart2pol(x, y, xs / 3,
+        if rrangein(x, y, xs / 3 + 100, ys / 2):
+            # normlistin.append(e(normlistinplaw[tally], cart2pol(x, y, xs / 3 + 100, ys / 2)[0], (cart2pol(x, y, xs / 3 + 100,
              # ys / 2)[1]) * 180 / math.pi, 20, 10, 3, 10))
-            normlistin.append(e(normlistinplaw[tally], cart2pol(x, y, xs / 3, ys / 2)[0], (cart2pol(x, y, xs / 3,
-            ys / 2)[1]), -90, float(parvaluesf[2]), 100, float(parvaluesf[3])))
+            normlistin.append(e(normlistinplaw[tally], cart2pol(x, y, xs / 3 + 100, ys / 2)[0], (cart2pol(x, y, xs / 3 + 100,
+            ys / 2)[1]), float(parvaluesf[3]), float(parvaluesf[4]), float(parvaluesf[2]), float(parvaluesf[5])))
             tally += 1
-        if rrangeout(x, y, xs / 3, ys / 2):
-            # normlistin.append(e(normlistinplaw[tally], cart2pol(x, y, xs / 3, ys / 2)[0], (cart2pol(x, y, xs / 3,
+        if rrangeout(x, y, xs / 3 + 100, ys / 2):
+            # normlistin.append(e(normlistinplaw[tally], cart2pol(x, y, xs / 3 + 100, ys / 2)[0], (cart2pol(x, y, xs / 3 + 100,
             #ys / 2)[1]) * 180 / math.pi, 20, 10, 3, 10))
-            normlistin.append(e(normlistinplaw[tally], cart2pol(x, y, xs / 3, ys / 2)[0], (cart2pol(x, y, xs / 3,
-            ys / 2)[1]), -90, float(parvaluesf[2]), 100, float(parvaluesf[3])))
+            normlistin.append(e(normlistinplaw[tally], cart2pol(x, y, xs / 3 + 100, ys / 2)[0], (cart2pol(x, y, xs / 3 + 100,
+            ys / 2)[1]), float(parvaluesf[3]), float(parvaluesf[4]), float(parvaluesf[2]), float(parvaluesf[5])))
             tally += 1
 # Figure out how to have spiral apply in the same way across this border - maybe I don't even need one?
 
@@ -191,24 +196,24 @@ print("min normlistout", min(normlistout))
 
 # Scales the normalized list to the range of the grayscale (0-255)
 tally = 0
-ycoord = 101
+ycoord = 201
 values = []
-img = Image.new("L", (1500, 1000))
+img = Image.new("L", (1800, 1400))
 draw = ImageDraw.Draw(img)
 [xs, ys] = rgb_im.size
 for x in range(1, xs):
     for y in range(1, ys):
-        if rrangein(x, y, xs / 3, ys / 2) and tally < len(normlistout):
+        if rrangein(x, y, xs / 3 + 100, ys / 2) and tally < len(normlistout):
             value = round(normlistout[tally] * 255)
             tally += 1
             values.append(value)
             img.putpixel((x, y), value)
-        if rrangeout(x, y, xs / 3, ys / 2) and tally < len(normlistout):
+        if rrangeout(x, y, xs / 3 + 100, ys / 2) and tally < len(normlistout):
             value = round(normlistout[tally] * 255)
             tally += 1
             values.append(value)
             img.putpixel((x, y), value)
-        if rrangein(x, y, xs / 3, ys / 2) == False and rrangeout(x, y, xs / 3, ys / 2) == False:
+        if rrangein(x, y, xs / 3 + 100, ys / 2) == False and rrangeout(x, y, xs / 3 + 100, ys / 2) == False:
             value = 255
             img.putpixel((x, y), value)
 
@@ -249,10 +254,10 @@ threequarter = "%.3f" % threequarter
 maximum = "%.3f" % maximum
 
 # Draw outline of bar graph
-x1 = 1200
-y1 = 100
-x2 = 1250
-y2 = 900
+x1 = 1450
+y1 = 200
+x2 = 1500
+y2 = 1200
 draw.rectangle((x1, y1, x2, y2), outline=0)
 width = x2 - x1
 height = y2 - y1
@@ -260,42 +265,49 @@ height = y2 - y1
 # Grabbing enough values to perfectly fill the bar graph, drawing lines of those values across the bar graph outline
 # Creates a linear gradient that aligns with the radial gradient plotted next to it
 x = 0
-while x < len(values) and ycoord < 900:
+while x < len(values) and ycoord < 1200:
     draw.line([x1 + 1, ycoord, x2 - 1, ycoord], fill=values[x], width=1)
     if x < qind:
         xq = x
-        ycoordq = 1000 - ycoord
+        ycoordq = 1400 - ycoord
     if x < hind:
         xh = x
-        ycoordh = 1000 - ycoord
+        ycoordh = 1400 - ycoord
     if x < tqind:
         xtq = x
-        ycoordtq = 1000 - ycoord
+        ycoordtq = 1400 - ycoord
     if x < mind:
         xm = x
-        ycoordm = 1000 - ycoord
+        ycoordm = 1400 - ycoord
     x += round(len(values) / height)
     ycoord += 1
 
 # print(xq, xh, xtq, xm)
 # print(ycoordq, ycoordh, ycoordtq, ycoordm)
 
-draw.line([1020, 0, 1020, 1000], fill=0, width=1)
+# Draw circles at limits to xi_b and ximax
+myfont = ImageFont.truetype("Times New Roman", 20)
+draw.ellipse([xs / 3 + 100 - xi_b, xs / 3 + 100 - xi_b, xs / 3 + 100 + xi_b, xs / 3 + 100 + xi_b])
+draw.ellipse([xs / 3 + 100 - ximax, xs / 3 + 100 - ximax, xs / 3 + 100 + ximax, xs / 3 + 100 + ximax])
+draw.text((920, 540), "xi_b", font=myfont, fill=0)
+draw.text((1080, 300), "xi_max", font=myfont, fill=0)
+
+# Draw lines that will align with labels created below
+draw.line([x1 - 230, 0, x1 - 230, 1400], fill=0, width=1)
 draw.line([x1 - 3, ycoordq, x2 + 3, ycoordq], fill=0, width=2)
 draw.line([x1 - 3, ycoordh, x2 + 3, ycoordh], fill=0, width=2)
 draw.line([x1 - 3, ycoordtq, x2 + 3, ycoordtq], fill=0, width=2)
 
 # Create font style and write labels in locations specified by pixel number
-myfont = ImageFont.truetype("Times New Roman", 20)
-draw.text((1195, 50), "B and L", font=myfont, fill=0)
-draw.text((1190, ycoordq), quarter, font=myfont, fill=0, anchor="rm")
-draw.text((1190, ycoordh), half, font=myfont, fill=0, anchor="rm")
-draw.text((1190, ycoordtq), threequarter, font=myfont, fill=0, anchor="rm")
-draw.text((1190, ycoordm), maximum, font=myfont, fill=0, anchor="rm")
-draw.text((1260, ycoordq), logq, font=myfont, fill=0, anchor="lm")
-draw.text((1260, ycoordh), logh, font=myfont, fill=0, anchor="lm")
-draw.text((1260, ycoordtq), logtq, font=myfont, fill=0, anchor="lm")
-draw.text((1260, ycoordm), logm, font=myfont, fill=0, anchor="lm")
+draw.text((x1 - 5, y1 - 50), "B and L", font=myfont, fill=0)
+draw.text((x1 - 30, ycoordq), quarter, font=myfont, fill=0, anchor="rm")
+draw.text((x1 - 30, ycoordh), half, font=myfont, fill=0, anchor="rm")
+draw.text((x1 - 30, ycoordtq), threequarter, font=myfont, fill=0, anchor="rm")
+draw.text((x1 - 30, ycoordm), maximum, font=myfont, fill=0, anchor="rm")
+draw.text((x2 + 10, ycoordq), logq, font=myfont, fill=0, anchor="lm")
+draw.text((x2 + 10, ycoordh), logh, font=myfont, fill=0, anchor="lm")
+draw.text((x2 + 10, ycoordtq), logtq, font=myfont, fill=0, anchor="lm")
+draw.text((x2 + 10, ycoordm), logm, font=myfont, fill=0, anchor="lm")
 
 # Create vertical bar graph labels
 blabelimg = Image.new("L", (180, 40), "white")
@@ -308,8 +320,21 @@ brotatedlabelimg = blabelimg.rotate(90.0, expand=1)
 lrotatedlabelimg = llabelimg.rotate(90.0, expand=1)
 
 # Paste new vertical labels into original image
-img.paste(brotatedlabelimg, (1040, 400))
-img.paste(lrotatedlabelimg, (1360, 350))
+img.paste(brotatedlabelimg, (x1 - 175, int((y2 - y1)/2) + 115))
+img.paste(lrotatedlabelimg, (x2 + 110, int((y2 - y1)/2) + 150))
+
+# Create axes and labels for axes
+draw.line([100, 100, 100, 1300], fill=0, width=2)
+draw.line([100, 1300, x1 - 280, 1300], fill=0, width=2)
+xlabelimg = Image.new("L", (180, 40), "white")
+ylabelimg = Image.new("L", (180, 40), "white")
+xlabeldraw = ImageDraw.Draw(xlabelimg)
+ylabeldraw = ImageDraw.Draw(ylabelimg)
+xlabeldraw.text((10, 20), "x label", font=myfont, fill=0, anchor="lm")
+ylabeldraw.text((10, 20), "y label", font=myfont, fill=0, anchor="lm")
+yrotatedlabelimg = ylabelimg.rotate(90.0, expand=1)
+img.paste(xlabelimg, (600, 1350))
+img.paste(yrotatedlabelimg, (50, 550))
 
 # Display and save the image
 img.show()
